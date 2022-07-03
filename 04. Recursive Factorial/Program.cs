c98daeaea7dbc0e01ba5a0e21dd4beff88554ea8
Angel Raychev <angelraychev@gmail.com>
2022-07-03 11:14:29 +0300

    Recursion and Backtracking task 4

---
+++ 04. Recursive Factorial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _04._Recursive_Factorial
+{
+    public class Program
+    {
+        static void Main(string[] args)
+       {
+            int n = int.Parse(Console.ReadLine());
+
+            long result = CalcFactorial(n);
+
+            Console.WriteLine(result);
+        }
+
+        private static long CalcFactorial(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+
+            return n * CalcFactorial(n - 1);
+        }
+    }
+}
