b186008ba227c9fd7029c8fb648948f8726d1e44
Angel Raychev <ily_g@abv.bg>
2022-07-04 16:03:51 +0300

    Recursion and Combinatorial Problems - Exercise task 2

---
+++ 02. Nested Loops/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _02._Nested_Loops
+{
+    public class Program
+    {
+        private static int[] elements;
+
+        public static void Main()
+        {
+            GetElements();
+            Combinations(new int[elements.Length], 0, 0); // n
+        }
+
+        private static void Combinations(int[] combination, int index, int border)
+        {
+            if (index == combination.Length)
+            {
+                Print(combination);
+            }
+            else
+            {
+                for (int current = 0; current < elements.Length; current++)
+                {
+                    combination[index] = elements[current];
+
+                    Combinations(combination, index + 1, current);
+                }
+            }
+        }
+
+        private static void Print(int[] combination)
+            => Console.WriteLine(string.Join(" ", combination));
+
+        private static void GetElements()
+        {
+            var n = int.Parse(Console.ReadLine());
+            elements = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                elements[i] = i + 1;
+            }
+        }
+    }
+}
