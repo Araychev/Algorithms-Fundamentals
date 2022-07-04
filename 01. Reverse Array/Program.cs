1d83cd43a0bc47e08b7a34493ee0d31994507380
Angel Raychev <ily_g@abv.bg>
2022-07-04 15:58:22 +0300

    Recursion and Combinatorial Problems - Exercise task 1

---
+++ 01. Reverse Array/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Reverse_Array
+{
+    public class Program
+    {
+        private static Stack<int> reversedArr = new Stack<int>();
+
+        public static void Main()
+        {
+            var arr = ReadArray();
+            Reverse(arr, 0);
+        }
+
+        private static void Reverse(int[] arr, int index)
+        {
+            if (index == arr.Length)
+            {
+                Console.WriteLine(string.Join(" ", reversedArr));
+            }
+            else
+            {
+                reversedArr.Push(arr[index]);
+
+                Reverse(arr, index + 1);
+            }
+        }
+
+        private static int[] ReadArray()
+            => Console.ReadLine()
+            .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+    }
+}
