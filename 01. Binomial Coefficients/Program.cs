2690aa7303a5b6895451f24c00f9c7ab09061eb9
Angel Raychev <ily_g@abv.bg>
2022-07-06 14:12:12 +0300

    Introduction to Dynamic Programming - Exercise task 1 added

---
+++ 01. Binomial Coefficients/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._Binomial_Coefficients
+{
+    public class Program
+    {
+        private static Dictionary<string, long> memo = new Dictionary<string, long>();
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+
+            long result = GetBinom(n, k);
+
+            Console.WriteLine(result);
+        }
+
+        private static long GetBinom(int row, int col)
+        {
+            var str = $"{row}|{col}";
+            if (memo.ContainsKey(str))
+            {
+                return memo[str];
+            }
+            if (row <= 1 || col == row || col < 1)
+            {
+                return 1;
+            }
+
+            var current = GetBinom(row - 1, col - 1) + GetBinom(row - 1, col);
+
+            memo.Add(str, current);
+
+            return current;
+        }
+    }
+}
