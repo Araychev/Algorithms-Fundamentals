33d63d202199a67e2bb9d0da7ed827ce338d2df1
Angel Raychev <ily_g@abv.bg>
2022-07-06 14:06:24 +0300

    Introduction to Dynamic Programming - Lab task 3 added

---
+++ 03. Longest Common Subsequence/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _03._Longest_Common_Subsequence
+{
+    public class Program
+    {
+
+        private static int[,] matrix;
+
+        static void Main(string[] args)
+        {
+            var firstString = Console.ReadLine();
+            var secondString = Console.ReadLine();
+
+            matrix = new int[firstString.Length + 1, secondString.Length + 1];
+
+            int result = FindLongestCommonSubsequence(firstString, secondString);
+
+            Console.WriteLine(result);
+        }
+
+        private static int FindLongestCommonSubsequence(string firstString, string secondString)
+        {
+            for (int i = 1; i < matrix.GetLength(0); i++)
+                for (int j = 1; j < matrix.GetLength(1); j++)
+                    matrix[i, j] = firstString[i - 1] == secondString[j - 1] ? matrix[i - 1, j - 1] + 1 : Math.Max(matrix[i - 1, j], matrix[i, j - 1]);
+
+            return matrix[matrix.GetLength(0) - 1, matrix.GetLength(1) - 1];
+        }
+    }
+}
