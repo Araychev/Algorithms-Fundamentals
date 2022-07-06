c06279a173b0fc84f7468fc6fa2dfab1169bf226
Angel Raychev <ily_g@abv.bg>
2022-07-06 10:39:58 +0300

    Graph Theory, Traversal and Shortest Paths task 1 added

---
+++ 01. Connected Components/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Connected_Components
+{
+    public class Program
+    {
+
+        private static List<int>[] graph;
+        private static bool[] visited;
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            graph = ReadGraph(n);
+            visited = new bool[n];
+
+            for (int i = 0; i < graph.Length; i++)
+            {
+                if (!visited[i])
+                {
+                    var list = Dfs(i, new List<int>());
+                    Console.WriteLine($"Connected component: {string.Join(" ", list)}");
+                    visited[i] = true;
+                }
+            }
+        }
+
+        private static List<int> Dfs(int node, List<int> result)
+        {
+            if (visited[node])
+            {
+                return result;
+            }
+
+            visited[node] = true;
+
+            foreach (var child in graph[node])
+            {
+                Dfs(child, result);
+            }
+
+            result.Add(node);
+
+            return result;
+        }
+
+        private static List<int>[] ReadGraph(int count)
+        {
+            var result = new List<int>[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                var str = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(str))
+                {
+                    result[i] = str
+                        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                        .Select(int.Parse)
+                        .ToList();
+                }
+                else
+                {
+                    result[i] = new List<int>();
+                }
+            }
+
+            return result;
+        }
+    }
+}
