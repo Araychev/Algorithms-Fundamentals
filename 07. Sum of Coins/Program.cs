a4de62f35283756788e7cef6b54987fd890290a5
Angel Raychev <ily_g@abv.bg>
2022-07-06 10:22:05 +0300

    Searching, Sorting and Greedy Algorithms Task 7 added

---
+++ 07. Sum of Coins/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace _07._Sum_of_Coins
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> coins = Console.ReadLine()
+                 .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                 .Select(int.Parse)
+                 .ToList();
+
+            var sum = int.Parse(Console.ReadLine());
+
+            coins.Sort();
+
+            var result = new List<Coins>();
+
+            while (coins.Count > 0)
+            {
+                int count = 0;
+                int value = coins[coins.Count - 1];
+
+                if (value <= sum)
+                {
+                    count = sum / value;
+                    sum = sum % value;
+                    result.Add(new Coins(value, count));
+                }
+
+                coins.RemoveAt(coins.Count - 1);
+            }
+
+            if (sum != 0)
+            {
+                Console.WriteLine("Error");
+            }
+            else
+            {
+                Console.WriteLine($"Number of coins to take: {result.Sum(v => v.Count)}");
+                result.ForEach(Console.WriteLine);
+            }
+        }
+    }
+
+        public class Coins
+        {
+            public Coins(int value, int count)
+            {
+                this.Value = value;
+                this.Count = count;
+            }
+
+            public int Value { get; set; }
+
+            public int Count { get; set; }
+
+            public override string ToString()
+            {
+                return $"{Count} coin(s) with value {Value}";
+            }
+        }
+    
+}
