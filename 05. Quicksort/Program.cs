fd6d02712eae881ca1815a8f7a634ff4fb07756a
Angel Raychev <ily_g@abv.bg>
2022-07-06 10:09:57 +0300

    Searching, Sorting and Greedy Algorithms Task 5 added

---
+++ 05. Quicksort/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Quicksort
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = Console.ReadLine()
+                 .Split()
+                 .Select(int.Parse)
+                 .ToList();
+
+            //QuickSort(arr, 0, arr.Length - 1);
+
+            arr.Sort();
+
+            Console.WriteLine(string.Join(" ", arr));
+        }
+
+        private static void QuickSort(int[] arr, int start, int end)
+        {
+            int i;
+            if (start < end)
+            {
+                i = Partition(arr, start, end);
+
+                QuickSort(arr, start, i - 1);
+                QuickSort(arr, i + 1, end);
+            }
+        }
+
+        private static int Partition(int[] arr, int start, int end)
+        {
+            int temp;
+            int p = arr[end];
+            int i = start - 1;
+
+            for (int j = start; j <= end - 1; j++)
+            {
+                if (arr[j] <= p)
+                {
+                    i++;
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+
+            temp = arr[i + 1];
+            arr[i + 1] = arr[end];
+            arr[end] = temp;
+            return i + 1;
+        }
+    }
+}
