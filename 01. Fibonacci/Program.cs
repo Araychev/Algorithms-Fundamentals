9a97a1757ca1d4bf0ccdb9bd3acd1399926b7ce5
Angel Raychev <ily_g@abv.bg>
2022-07-06 13:52:49 +0300

    Introduction to Dynamic Programming - Lab task 1 added

---
+++ 01. Fibonacci/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _01._Fibonacci
+{
+    public class Program
+    {
+
+        private static long[] memo;
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            memo = new long[n + 1];
+
+            long result = Fibonacci(n);
+
+            Console.WriteLine(result);
+        }
+
+        private static long Fibonacci(int number)
+        {
+            if (memo[number] > 0)
+            {
+                return memo[number];
+            }
+
+            if (number <= 2)
+            {
+                return 1;
+            }
+
+            memo[number] = Fibonacci(number - 1) + Fibonacci(number - 2);
+
+            return memo[number];
+        }
+    }
+}
