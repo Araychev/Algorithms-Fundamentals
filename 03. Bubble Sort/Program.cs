303709594e900cb40373ec17140681e395cbc220
Angel Raychev <ily_g@abv.bg>
2022-07-06 10:05:19 +0300

    Searching, Sorting and Greedy Algorithms Task 3 added

---
+++ 03. Bubble Sort/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Bubble_Sort
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            BubbleSort(arr);
+
+            Console.WriteLine(string.Join(" ", arr));
+        }
+
+        private static void BubbleSort(int[] arr)
+        {
+            bool IsSorted = false;
+            int firstIndex = 0;
+
+            while (!IsSorted)
+            {
+                IsSorted = true;
+
+                for (int i = firstIndex + 1; i < arr.Length; i++)
+                {
+                    if (arr[i - 1] > arr[i])
+                    {
+                        Swap(arr, i - 1, i);
+                        IsSorted = false;
+                    }
+                }
+            }
+        }
+
+        private static void Swap(int[] arr, int first, int second)
+        {
+            var temp = arr[first];
+            arr[first] = arr[second];
+            arr[second] = temp;
+        }
+    }
+}
