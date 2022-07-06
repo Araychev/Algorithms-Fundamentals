ceb22caf96a8fe930c24789f9a30cd1b8e1da847
Angel Raychev <ily_g@abv.bg>
2022-07-06 10:45:23 +0300

    Graph Theory, Traversal and Shortest Paths task 2 added

---
+++ 02. Topological Sorting/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Topological_Sorting
+{
+    public class Program
+    {
+        private static Dictionary<string, List<string>> graph = new Dictionary<string, List<string>>();
+        private static Dictionary<string, int> dependencies = new Dictionary<string, int>();
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            ReadGraph(n);
+
+            FindDependencies();
+
+            Sort();
+        }
+
+        private static void Sort()
+        {
+            var sorted = new List<string>();
+
+            while (dependencies.Count != 0)
+            {
+                var node = dependencies
+                    .FirstOrDefault(k => k.Value == 0);
+
+                if (node.Key == null)
+                {
+                    break;
+                }
+
+                foreach (var child in graph[node.Key])
+                {
+                    dependencies[child]--;
+                }
+
+                sorted.Add(node.Key);
+                dependencies.Remove(node.Key);
+
+            }
+
+            if (dependencies.Count != 0)
+            {
+                Console.WriteLine("Invalid topological sorting");
+            }
+            else
+            {
+                Console.WriteLine($"Topological sorting: {string.Join(", ", sorted)}");
+            }
+        }
+
+        private static void FindDependencies()
+        {
+            foreach (var node in graph)
+            {
+
+                var key = node.Key;
+                var children = node.Value;
+
+                if (!dependencies.ContainsKey(key))
+                {
+                    dependencies.Add(key, 0);
+                }
+
+                foreach (var child in children)
+                {
+                    if (!dependencies.ContainsKey(child))
+                    {
+                        dependencies.Add(child, 0);
+                    }
+
+                    dependencies[child] += 1;
+                }
+            }
+        }
+
+        private static void ReadGraph(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                var line = Console.ReadLine()
+                    .Split("->", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                var key = line[0].TrimEnd();
+                if (line.Length == 1)
+                {
+                    graph.Add(key, new List<string>());
+                }
+                else
+                {
+                    var children = line[1]
+                        .TrimStart()
+                        .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                        .ToList();
+
+                    graph.Add(key, children);
+                }
+            }
+        }
+    }
+}
