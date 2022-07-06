b9962ea89d6a56a78196b6bec889e5e5752a289f
Angel Raychev <ily_g@abv.bg>
2022-07-06 09:51:55 +0300

    Recursion and Combinatorial Problems - Exercise task 5 added

---
+++ 05. School Teams/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._School_Teams
+{
+    internal class Program
+    {
+        private static string[] girls;
+        private static string[] boys;
+
+        static void Main()
+        {
+            girls = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            boys = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            GenerateCombinations(new List<string>(), 0, 0);
+        }
+
+        private static void GenerateCombinations(List<string> result, int index, int iterator)
+        {
+            if (index >= 3)
+            {
+                GenerateCombinationsWithBoys(result, new List<string>(), 0, 0);
+                return;
+            }
+
+            for (int i = iterator; i < girls.Length; i++)
+            {
+                result.Add(girls[i]);
+                GenerateCombinations(result, index + 1, i + 1);
+                result.Remove(girls[i]);
+            }
+        }
+
+        private static void GenerateCombinationsWithBoys(List<string> girlsResult, List<string> boysResult, int index, int iterator)
+        {
+            if (index >= 2)
+            {
+                Console.WriteLine(string.Join(", ", girlsResult) + ", " + string.Join(", ", boysResult));
+                return;
+            }
+
+            for (int i = iterator; i < boys.Length; i++)
+            {
+                boysResult.Add(boys[i]);
+                GenerateCombinationsWithBoys(girlsResult, boysResult, index + 1, i + 1);
+                boysResult.Remove(boys[i]);
+            }
+        }
+    }
+}
