5e3b9a438953f2ebc0bdb5b612786b1b674a468a
Angel Raychev <ily_g@abv.bg>
2022-07-06 11:26:08 +0300

    Graph Theory, Traversal and Shortest Paths - Exercise task 2 and 3 added

---
+++ 02. Areas in Matrix/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Areas_in_Matrix
+{
+    public class Program
+    {
+        private static string[,] matrix;
+        private static bool[,] visited;
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+
+            visited = new bool[n, m];
+            matrix = ReadMatrix(n, m);
+
+            Dictionary<string, int> areas = new Dictionary<string, int>();
+
+            var aresCount = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+
+                    if (!visited[i, j])
+                    {
+                        Dfs(i, j);
+
+                        var element = matrix[i, j];
+                        if (!areas.ContainsKey(element))
+                        {
+                            areas[element] = 0;
+                        }
+
+                        areas[element]++;
+                        aresCount++;
+                    }
+                }
+            }
+
+            var sorted = areas
+                .OrderBy(k => k.Key);
+
+            Console.WriteLine($"Areas: {aresCount}");
+
+            foreach (var kvp in sorted)
+            {
+                Console.WriteLine($"Letter '{kvp.Key}' -> {kvp.Value}");
+            }
+        }
+
+        public class Child
+        {
+            public Child(int row, int col)
+            {
+                this.Row = row;
+                this.Col = col;
+            }
+
+            public int Row { get; set; }
+
+            public int Col { get; set; }
+        }
+
+        private static void Dfs(int row, int col)
+        {
+            visited[row, col] = true;
+
+            List<Child> children = FindAllValidChildren(row, col);
+
+            foreach (var child in children)
+            {
+                Dfs(child.Row, child.Col);
+            }
+        }
+
+        private static List<Child> FindAllValidChildren(int row, int col)
+        {
+            var list = new List<Child>();
+
+            if (IsInMatrix(row + 1, col) && matrix[row, col] == matrix[row + 1, col] && !visited[row + 1, col])
+            {
+                list.Add(new Child(row + 1, col));
+            }
+
+            if (IsInMatrix(row - 1, col) && matrix[row, col] == matrix[row - 1, col] && !visited[row - 1, col])
+            {
+                list.Add(new Child(row - 1, col));
+            }
+
+            if (IsInMatrix(row, col + 1) && matrix[row, col] == matrix[row, col + 1] && !visited[row, col + 1])
+            {
+                list.Add(new Child(row, col + 1));
+            }
+
+            if (IsInMatrix(row, col - 1) && matrix[row, col] == matrix[row, col - 1] && !visited[row, col - 1])
+            {
+                list.Add(new Child(row, col - 1));
+            }
+
+            return list;
+        }
+
+        private static bool IsInMatrix(int row, int col)
+        {
+            return row >= 0 &&
+                   row < matrix.GetLength(0) &&
+                   col >= 0 &&
+                   col < matrix.GetLength(1);
+        }
+
+        private static string[,] ReadMatrix(int n, int m)
+        {
+            var result = new string[n, m];
+
+            for (int i = 0; i < n; i++)
+            {
+                string line = Console.ReadLine();
+
+                for (int j = 0; j < m; j++)
+                {
+                    result[i, j] = line[j].ToString();
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ 03. Cycles in Graph/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Cycles_in_Graph
+{
+    public class Program
+    {
+        private static Dictionary<string, List<string>> graph = new Dictionary<string, List<string>>();
+        private static bool cyclic = false;
+        private static HashSet<string> visited = new HashSet<string>();
+
+        static void Main(string[] args)
+        {
+            var input = "";
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+                var nodes = input
+                    .Split("-", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                var from = nodes[0];
+                var to = nodes[1];
+
+                if (!graph.ContainsKey(from))
+                {
+                    graph[from] = new List<string>();
+                }
+
+                graph[from].Add(to);
+            }
+
+            foreach (var kvp in graph)
+            {
+                Dfs(kvp.Key);
+            }
+
+            if (!cyclic)
+            {
+                Console.WriteLine("Acyclic: Yes");
+            }
+            else
+            {
+                Console.WriteLine("Acyclic: No");
+            }
+        }
+
+        private static void Dfs(string node)
+        {
+            if (visited.Contains(node))
+            {
+                cyclic = true;
+                return;
+            }
+
+            visited.Add(node);
+
+            if (graph.ContainsKey(node))
+            {
+                foreach (var child in graph[node])
+                {
+                    Dfs(child);
+                    visited.Remove(child);
+                }
+            }
+        }
+    }
+}
