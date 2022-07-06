a3e8058ccfa7669acbef5c57c1d89db1a499f3ce
Angel Raychev <ily_g@abv.bg>
2022-07-06 11:30:40 +0300

    Graph Theory, Traversal and Shortest Paths - Exercise task 4 added

---
+++ 04. Salaries/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04._Salaries
+{
+    public class Program
+    {
+        private static List<int>[] graph;
+        private static Dictionary<int, int> salaries = new Dictionary<int, int>();
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            graph = new List<int>[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                var line = Console.ReadLine();
+                graph[i] = new List<int>();
+
+                for (int j = 0; j < n; j++)
+                {
+                    if (line[j] == 'Y')
+                    {
+                        graph[i].Add(j);
+                    }
+                }
+            }
+
+            var totalSalary = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                totalSalary += Dfs(i);
+            }
+
+            Console.WriteLine(totalSalary);
+        }
+        private static int Dfs(int node)
+        {
+            if (graph[node].Count == 0)
+            {
+                return 1;
+            }
+
+            var salary = 0;
+            foreach (var child in graph[node])
+            {
+                if (salaries.ContainsKey(child))
+                {
+                    salary += salaries[child];
+                }
+                else
+                {
+                    salary += Dfs(child);
+                }
+            }
+
+            salaries[node] = salary;
+            return salary;
+        }
+    }
+}
