b40866a40991d984e97ea5b7eb0743f5ede1c16e
Angel Raychev <ily_g@abv.bg>
2022-07-06 14:18:28 +0300

    Introduction to Dynamic Programming - Exercise task 2 added

---
+++ 02. Dividing Presents/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Dividing_Presents
+{
+    public class Program
+    {
+        private static Dictionary<int, int> memo = new Dictionary<int, int>();
+
+        static void Main(string[] args)
+        {
+            var presents = Console.ReadLine()
+                 .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                 .Select(int.Parse)
+                 .ToArray();
+
+            GetSums(presents);
+
+            var middle = (int)Math.Ceiling(presents.Sum() / 2.0);
+
+            var bobPoints = -1;
+
+            while (bobPoints < 0)
+            {
+                if (memo.ContainsKey(middle))
+                {
+                    bobPoints = middle;
+                    break;
+                }
+
+                middle++;
+            }
+
+            var alanPoints = presents.Sum() - bobPoints;
+
+            var alanPresents = new List<int>();
+
+            Console.WriteLine($"Difference: {bobPoints - alanPoints}");
+            Console.WriteLine($"Alan:{alanPoints} Bob:{bobPoints}");
+
+            while (alanPoints > 0)
+            {
+                alanPresents.Add(alanPoints - memo[alanPoints]);
+                alanPoints = memo[alanPoints];
+            }
+
+            Console.WriteLine($"Alan takes: {string.Join(" ", alanPresents)}");
+            Console.WriteLine("Bob takes the rest.");
+        }
+
+
+        private static void GetSums(int[] presents)
+        {
+            memo.Add(0, 0);
+
+            foreach (var present in presents)
+            {
+                var keys = memo.Keys.ToArray();
+                foreach (var key in keys)
+                {
+                    var sum = key + present;
+                    if (!memo.ContainsKey(sum))
+                    {
+                        memo.Add(sum, key);
+                    }
+                }
+            }
+        }
+    }
+}
