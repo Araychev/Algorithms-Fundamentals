8eede4f87d1ff9d7b971e6733cb42035898b65ef
Angel Raychev <ily_g@abv.bg>
2022-07-06 14:30:41 +0300

    Introduction to Dynamic Programming - Exercise task 4 added

---
+++ 04. Sum with Limited Coins/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Sum_with_Limited_Coins
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var coins = Console.ReadLine()
+               .Split(" ")
+               .Select(int.Parse)
+               .ToArray();
+
+            var sum = int.Parse(Console.ReadLine());
+
+            var count = GetCount(coins, sum);
+
+            Console.WriteLine(count);
+        }
+
+        private static int GetCount(int[] coins, int sum)
+        {
+            int count = 0;
+
+            HashSet<int> sums = new HashSet<int>();
+            sums.Add(0);
+
+            foreach (var coin in coins)
+            {
+                var temp = new HashSet<int>(sums);
+
+                foreach (var current in temp)
+                {
+                    sums.Add(current + coin);
+                    if (current + coin == sum)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+    }
+}
