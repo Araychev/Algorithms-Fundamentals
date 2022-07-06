e7aabbee7c77ab9629f56f9ceafaf9c5ad55a79f
Angel Raychev <ily_g@abv.bg>
2022-07-06 14:24:13 +0300

    Introduction to Dynamic Programming - Exercise task 3 added

---
+++ 03. Sum with Unlimited Coins/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Sum_with_Unlimited_Coins
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var coins = Console.ReadLine()
+                 .Split(" ")
+                 .Select(int.Parse)
+                 .ToArray();
+
+            var sum = int.Parse(Console.ReadLine());
+
+            int count = GetCount(coins, sum);
+
+            Console.WriteLine(count);
+        }
+
+        private static int GetCount(int[] coins, int sum)
+        {
+            var sums = new int[sum + 1];
+            sums[0] = 1;
+
+            for (int i = 0; i < coins.Length; i++)
+            {
+                for (int j = coins[i]; j < sums.Length; j++)
+                {
+                    sums[j] += sums[j - coins[i]];
+                }
+            }
+
+            return sums[sums.Length - 1];
+        }
+    }
+}
