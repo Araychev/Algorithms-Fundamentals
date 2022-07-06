661b3212eb516a63a4b9d8e8c494c7e979f949c5
Angel Raychev <ily_g@abv.bg>
2022-07-06 10:01:02 +0300

    Searching, Sorting and Greedy Algorithms Task 2 added

---
+++ 02. Selection Sort/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Selection_Sort
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+               .Split()
+               .Select(int.Parse)
+               .ToArray();
+
+            SelectionSort(arr);
+
+            Console.WriteLine(string.Join(" ", arr));
+        }
+
+        private static void SelectionSort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        Swap(arr, i, j);
+                    }
+                }
+            }
+        }
+
+        private static void Swap(int[] arr, int first, int second)
+        {
+            var temp = arr[first];
+            arr[first] = arr[second];
+            arr[second] = temp;
+        }
+    }
+}
