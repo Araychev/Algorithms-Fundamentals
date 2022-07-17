12938848e34dfe91e38ac11e7528b0e788818e27
Angel Raychev <angelraychev@gmail.com>
2022-07-17 11:13:21 +0300

    Exam prep task 1 added

---
+++ 01. Two Minutes to Midnight Exam Prep/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Numerics;
+
+namespace _01._Two_Minutes_to_Midnight_Exam_Prep
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+
+            BigInteger binomalCoeficient = GetFactorial(n) / (GetFactorial(k) * GetFactorial(n - k));
+
+            Console.WriteLine(binomalCoeficient);
+        }
+
+        private static BigInteger GetFactorial(int i)
+        {
+            BigInteger factorial = 1;
+            for (int j = 2; j <= i; j++)
+            {
+                factorial *= j;
+            }
+            return factorial;
+        }
+    }
+}
